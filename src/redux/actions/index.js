eb8b90d78507814553c0bfba4206c3d18b4a2059
Reid Delahunt <reidpierredelahunt@gmail.com>
2017-09-15 13:45:38 -0700

    search action for redux-form-saga

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,14 +1,21 @@
-// ACTIONS GO HERE
+/*
 
-const spotify = require('../../config/spotifyWebApi.js');
+This file is responsible for hosting pure action-creator functions
+for changing the state of redux reducers with their payload.
 
+*/
+
+// const spotify = require('../../config/spotifyWebApi.js');
+import { createFormAction } from 'redux-form-saga';
 // our constants
-export const SPOTIFY_TOKENS = 'SPOTIFY_TOKENS';
-export const SPOTIFY_ME_BEGIN = 'SPOTIFY_ME_BEGIN';
-export const SPOTIFY_ME_SUCCESS = 'SPOTIFY_ME_SUCCESS';
-export const SPOTIFY_ME_FAILURE = 'SPOTIFY_ME_FAILURE';
+// export const SPOTIFY_TOKENS = 'SPOTIFY_TOKENS';
+// export const SPOTIFY_ME_BEGIN = 'SPOTIFY_ME_BEGIN';
+// export const SPOTIFY_ME_SUCCESS = 'SPOTIFY_ME_SUCCESS';
+// export const SPOTIFY_ME_FAILURE = 'SPOTIFY_ME_FAILURE';
 export const SET_USER_INFO = 'SET_USER_INFO';
 export const SET_TOKENS = 'SET_TOKENS';
+
+export const searchBarSubmit = createFormAction('SEARCH');
 
 /** set the app's access and refresh tokens */
 export const setTokens = ({access_token, refresh_token, expires_in}) => {
