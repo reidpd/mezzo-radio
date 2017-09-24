64aa8863c01113bfae3a7422fce9f8a4f0e1e7af
Reid Delahunt <reidpierredelahunt@gmail.com>
2017-09-24 15:50:04 -0700

    documentation for rewind_song

--- src/components/record/player/controls/rewind_song/index.js
+++ src/components/record/player/controls/rewind_song/index.js
@@ -4,8 +4,6 @@ This component is responsible for housing the button which will give the user th
 functionality of rewinding to the beginning of a selected 'track/single'
 from the selected 'record/disk' on the 'record/player/platter'.
 
-Its future existence is still TBD.
-
 */
 
 import React, { Component } from 'react';
