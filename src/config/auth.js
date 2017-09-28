ed51df3126ac286b704c852753bc7db2a4e52e59
Reid Delahunt <reidpierredelahunt@gmail.com>
2017-09-28 15:22:50 -0700

    first attempt at implementing environment routing with oauth

--- src/config/auth.js
+++ src/config/auth.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const clientId = process.env.SPOTIFY_CLIENT_ID || process.env.REACT_APP_SPOTIFY_CLIENT_ID;
 const clientSecret = process.env.SPOTIFY_CLIENT_SECRET || process.env.REACT_APP_SPOTIFY_CLIENT_SECRET;
 const redirectUri = process.env.SPOTIFY_CALLBACK_URL || process.env.REACT_APP_SPOTIFY_CALLBACK_URL;
+console.log(process.env);
 
 const credentials = { clientId, clientSecret, redirectUri, passReqToCallback: true };
 
